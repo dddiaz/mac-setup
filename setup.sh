13940836956fd8c4397f8e880301d9c5c97b5da7
Daniel Diaz <3068333+dddiaz@users.noreply.github.com>
2019-09-16 15:54:08 -0700

    updated shell script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 # Mac Setup Script
 # Daniel Diaz 2019
-# Note: Homebrew should not be installed as root user.
 
 echo "Installing xcode-select"
 xcode-select --install
@@ -20,12 +19,26 @@ brew update
 # git config --global user.name "Daniel Diaz"
 # git config --global user.email <EMAIL>
 
+echo "Making src directory to store source code."
+mkdir -p ~/src
+
 echo "Installing other brew stuff..."
-# Install Python3
-brew install python3
 # Nifty http lib
 brew install httpie
 brew install node
+brew install kubernetes-cli
+brew install terraform
+brew install warrensbox/tap/tfswitch
+
+echo "Installing Python 3"
+# Install Python3
+brew install python3
+
+echo "Install python dependent packages"
+# Install awscli with python3 not python 2
+pip3 install awscli --user
+# Install ansible
+pip3 install ansible --user
 
 echo "Cleaning up brew"
 brew cleanup
@@ -33,68 +46,47 @@ brew cleanup
 echo "Installing homebrew cask"
 brew tap caskroom/cask
 
+echo "Installing Cask Apps"
+brew cask install docker
+brew cask install jetbrains-toolbox
+brew cask install iterm2
+
 # echo "Copying dotfiles from Github"
 # cd ~
-# git clone git@github.com:bradp/dotfiles.git .dotfiles
+# git clone git@github.com:dddiaz/dotfiles.git .dotfiles
 # cd .dotfiles
 # sh symdotfiles
 
-# #Install Zsh & Oh My Zsh
-# echo "Installing Oh My ZSH..."
-# curl -L http://install.ohmyz.sh | sh
+#Install Zsh & Oh My Zsh
+echo "Installing Oh My ZSH..."
+curl -L http://install.ohmyz.sh | sh
 
 # echo "Setting up Oh My Zsh theme..."
 # cd  /Users/bradparbs/.oh-my-zsh/themes
 # curl https://gist.githubusercontent.com/bradp/a52fffd9cad1cd51edb7/raw/cb46de8e4c77beb7fad38c81dbddf531d9875c78/brad-muse.zsh-theme > brad-muse.zsh-theme
 
-# echo "Setting up Zsh plugins..."
-# cd ~/.oh-my-zsh/custom/plugins
-# git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "Setting up Zsh plugins..."
+cd ~/.oh-my-zsh/custom/plugins
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 
-# echo "Setting ZSH as shell..."
-# chsh -s /bin/zsh
+echo "Setting ZSH as shell..."
+chsh -s /bin/zsh
 
-# # Apps
-# apps=(
-#   alfred
-#   bartender
-#   bettertouchtool
-#   cleanmymac
-#   cloud
-#   colloquy
-#   cornerstone
-#   diffmerge
-#   dropbox
-#   filezilla
-#   firefox
-#   google-chrome
-#   harvest
-#   hipchat
-#   licecap
-#   mou
-#   phpstorm
-#   private-internet-access
-#   razer-synapse
-#   sourcetree
-#   steam
-#   spotify
-#   vagrant
-#   iterm2
-#   sublime-text2
-#   textexpander
-#   virtualbox
-#   mailbox
-#   vlc
-#   skype
-#   transmission
-#   zoomus
-#   onepassword
-#   sequel-pro
-#   chromecast
-#   qlmarkdown
-#   qlstephen
-#   suspicious-package
-# )
+# Apps
+apps=(
+  aws
+  ansible
+  brew
+  docker
+  diffmerge
+  google-chrome
+  gitignore
+  helm
+  iterm2
+  kubectl
+  onepassword
+  osx
+)
 
 
 echo "Setting some Mac settings..."
@@ -111,7 +103,7 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 #"Use column view in all Finder windows by default"
 defaults write com.apple.finder FXPreferredViewStyle Clmv
 
-
+# TODO: move app bar to left
 
 
 
